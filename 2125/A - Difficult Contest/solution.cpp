e973d1ce5b44a450efbb8ce8b919b0e499acf105
ANKIT KUMAR <ankit2515ry@gmail.com>
2026-07-09 16:19:17 +0530

    Add Difficult Contest [A] from Codeforces

---
+++ 2125/A - Difficult Contest/solution.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    reverse(s.begin(),s.end());
+    cout<<s<<endl;
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
